f7723d771fb3a29c34296e9abfcb376046974949
Philipp Hansch <dev@phansch.net>
2020-07-09 09:26:54 +0200

    Basic incrementing timer

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,43 @@
-use iced::{button, Button, Column, Element, Sandbox, Settings, Text};
-use std::time::Duration;
+use iced::{button, executor, time, Application, Button, Column, Command, Element, Settings, Subscription, Text};
+use std::time::{Duration, Instant};
 
-#[derive(Default)]
 struct Pomo {
-    elapsed: Option<Duration>,
+    elapsed: Duration,
+    state: PomoState,
 
     // The local state of the two buttons
     cancel_button: button::State,
     start_button: button::State,
 }
 
+enum PomoState {
+    Idle,
+    Ticking { last_tick: Instant }
+}
+
 #[derive(Debug, Clone, Copy)]
 pub enum Message {
     StartPressed,
+    Tick(Instant),
     CancelPressed,
 }
 
 
-impl Sandbox for Pomo {
-
+impl Application for Pomo {
+    type Executor = executor::Default;
     type Message = Message;
+    type Flags = ();
 
-    fn new() -> Self {
-        Self::default()
+    fn new(_flags: ()) -> (Pomo, Command<Message>) {
+        (
+            Pomo {
+                elapsed: Duration::default(),
+                state: PomoState::Idle,
+                cancel_button: button::State::new(),
+                start_button: button::State::new(),
+            },
+            Command::none()
+        )
     }
 
     fn title(&self) -> String {
@@ -40,10 +55,7 @@ impl Sandbox for Pomo {
             )
             .push(
                 // We show the value of the counter here
-                match &self.elapsed {
-                    Some(val) => Text::new(val.as_secs().to_string()).size(50),
-                    None => Text::new("not started")
-                }
+                Text::new(&self.elapsed.as_secs().to_string()).size(50),
             )
             .push(
                 // The decrement button. We tell it to produce a
@@ -54,13 +66,39 @@ impl Sandbox for Pomo {
             .into()
     }
 
-    fn update(&mut self, message: Message) {
+    fn update(&mut self, message: Message) -> Command<Message> {
         match message {
             Message::StartPressed => {
-                self.elapsed = Some(Duration::new(0, 0));
+                match self.state {
+                    PomoState::Idle => {
+                        self.state = PomoState::Ticking {
+                            last_tick: Instant::now(),
+                        }
+                    },
+                    _ => {}
+                }
+            },
+            Message::Tick(now) => {
+                match &mut self.state {
+                    PomoState::Ticking { last_tick } => {
+                        self.elapsed += now - *last_tick;
+                        *last_tick = now;
+                    },
+                    _ => {}
+                }
             }
             Message::CancelPressed => {
-                self.elapsed = None;
+                self.elapsed = Duration::default();
+            }
+        }
+        Command::none()
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        match self.state {
+            PomoState::Idle => Subscription::none(),
+            PomoState::Ticking { .. } => {
+                time::every(Duration::from_millis(10)).map(Message::Tick)
             }
         }
     }
